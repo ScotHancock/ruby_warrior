1181a15dc355fa068187d862fd4cd95a5298edad
Scot Hancock <scot@onespark.com>
2015-02-16 13:15:10 -0500

    lvl3

--- player.rb
+++ player.rb
@@ -1,7 +1,10 @@
 class Player
   def play_turn(warrior)
+	
   	if warrior.feel.enemy?
-   		warrior.attack!
+  		warrior.attack!
+	elsif warrior.health < 20 
+		warrior.rest!
    	else
    		warrior.walk!
    	#we changed this
