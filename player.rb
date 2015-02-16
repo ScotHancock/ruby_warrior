bdccdc752a84ad5485765e383a7dcdef0f8664d0
Scot Hancock <scot@onespark.com>
2015-02-16 14:07:04 -0500

    lvl5

--- player.rb
+++ player.rb
@@ -1,13 +1,29 @@
 class Player
+  	MAX_HEALTH = 20
+	LOW_HEALTH = 20
+
+  
   def play_turn(warrior)
-	
+  	# Ruby or Equal operator. If @health is defined (has a value), don;t do anything
+  	# If it's nil (hasnt been assigned a value), assign the value.
+  	@health ||= MAX_HEALTH
+
   	if warrior.feel.enemy?
   		warrior.attack!
-	elsif warrior.health < 20 
-		warrior.rest!
+  		
+  	elsif warrior.health < @health
+  		warrior.walk!
+  	elsif warrior.health < MAX_HEALTH
+  		warrior.rest!
+  	
+	
    	else
    		warrior.walk!
    	#we changed this
    	end
+   	
+   	@health = warrior.health
+   	#Concatenate the ruby way
+   	puts "Current Health #{@health}"
   end
 end
